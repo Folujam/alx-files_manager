735f146939a5e5bc23cb2fe5d8c886a57f7f744e
folujam <olusway@gmail.com>
2024-09-03 20:09:11 +0100

    es6 check

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
+import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor () {
@@ -31,4 +31,4 @@ class DBClient {
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+export default dbClient;
