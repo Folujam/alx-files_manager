2e7d85eb2e6b8c37920a7754252a27cc24b94470
folujam <olusway@gmail.com>
2024-09-03 15:38:49 +0100

    semistandard compliant

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 
 class DBClient {
-  constructor() {
+  constructor () {
     this.url = `mongodb://${process.env.DB_HOST || 'localhost'}:${process.env.DB_PORT || 27017}`;
     this.dbName = process.env.DB_DATABASE || 'files_manager';
     this.client = new MongoClient(this.url, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,17 +13,17 @@ class DBClient {
     });
   }
 
-  isAlive() {
+  isAlive () {
     return this.client.topology.isConnected();
   }
 
-  async nbUsers() {
+  async nbUsers () {
     const db = this.client.db(this.dbName);
     const collection = db.collection('users');
     return await collection.countDocuments();
   }
 
-  async nbFiles() {
+  async nbFiles () {
     const db = this.client.db(this.dbName);
     const collection = db.collection('files');
     return await collection.countDocuments();
