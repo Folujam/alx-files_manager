0f4b2e8a3b2ef3da660018c7bf2465103fb1af85
folujam <olusway@gmail.com>
2024-09-03 21:01:11 +0100

    es6 check

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-  constructor () {
+  constructor() {
     this.url = `mongodb://${process.env.DB_HOST || 'localhost'}:${process.env.DB_PORT || 27017}`;
     this.dbName = process.env.DB_DATABASE || 'files_manager';
     this.client = new MongoClient(this.url, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,20 +13,20 @@ class DBClient {
     });
   }
 
-  isAlive () {
+  isAlive() {
     return this.client.topology.isConnected();
   }
 
-  async nbUsers () {
+  async nbUsers() {
     const db = this.client.db(this.dbName);
     const collection = db.collection('users');
-    return await collection.countDocuments();
+    return collection.countDocuments();
   }
 
-  async nbFiles () {
+  async nbFiles() {
     const db = this.client.db(this.dbName);
     const collection = db.collection('files');
-    return await collection.countDocuments();
+    return collection.countDocuments();
   }
 }
 
